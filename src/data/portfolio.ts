37b71b63d8c2638dbc89885f14b515e8970e7101
Chad <sclanga315@gmail.com>
2026-04-01 23:40:39 -0400

    Remove Diabetes IoB project from portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -28,12 +28,4 @@ export const portfolioData: Portfolio[] = [
     codeUrl: "https://colab.research.google.com/drive/1RjVy8d1PUhN25oEpLf3F4rjkP7cWFZBX?usp=sharing",
     //imageUrl: "/images/music_recommendations.jpg",
   },
-  {
-    title: "Diabetes Management Simulation Using Digital Twins and the Internet of Behaviors (IoB)",
-    description:
-      "A simulation project leveraging Digital Twins and IoB to explore and optimize interventions for type 2 diabetes patients. The project compares traditional methods with IoB-enhanced strategies, showcasing metrics such as A1C trajectories and behavioral insights.",
-    technologies: ["Python", "Colab", "IoB"],
-    codeUrl: "https://colab.research.google.com/drive/1K3Z1lkURtew8eeRYd3mlgblnNxSro9nk?usp=sharing",
-    //imageUrl: "/images/diabetes_simulation.jpg",
-  },
 ];

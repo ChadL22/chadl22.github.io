31df24fd684b605127475fca09b6dddd6818289f
Chad <sclanga315@gmail.com>
2026-04-01 23:37:19 -0400

    Update Misinformation project written URL to Google Doc

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -14,7 +14,7 @@ export const portfolioData: Portfolio[] = [
     description:
       "An interactive notebook exploring the societal problem of misinformation through the lenses of philosophy, mathematics, and societal systems. This project proposes interdisciplinary approaches to improve online communities using classification techniques.",
     technologies: ["Python", "Colab", "Classification Theory"],
-    writtenUrl: "https://phronesisresearch.org/posts/misinformation",
+    writtenUrl: "https://docs.google.com/document/d/1_WIlHyZYe5LQ7pLxFgsb5EO4yNJXlxFb/edit?usp=sharing&ouid=113622749755892142878&rtpof=true&sd=true",
     codeUrl: "https://colab.research.google.com/drive/1kyAcuL7FNCDhKT3SIKDiXYrnuKTe650v?usp=sharing",
     //imageUrl: "/images/misinformation.jpg",
   },
@@ -23,7 +23,7 @@ export const portfolioData: Portfolio[] = [
     description:
       "A Google Colab notebook proposing an algorithmic framework inspired by Szemerédi's Theorem to break repetitive patterns in music recommendations. This project aims to enhance diversity and serendipity while maintaining user satisfaction on Digital Streaming Platforms. This work is now being contributed to Parachord as a plugin.",
     technologies: ["Python", "Colab", "Szemerédi's Theorem"],
-    writtenUrl: "https://phronesisresearch.org/posts/breaking-patterns",
+    writtenUrl: "https://docs.google.com/document/d/1JYxX8S7rYhQdkiPSjmJgTpt8XGWvG4JPoeDBlgCPREU/edit?usp=sharing",
     projectUrl: "https://parachord.com/",
     codeUrl: "https://colab.research.google.com/drive/1RjVy8d1PUhN25oEpLf3F4rjkP7cWFZBX?usp=sharing",
     //imageUrl: "/images/music_recommendations.jpg",
